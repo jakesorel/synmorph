c92ce1d97ac24d687b856405f5dcb933026627b5
jakesorel <jake.cornwallscoones@crick.ac.uk>
2022-03-23 22:07:16 +0000

    ave_branch

--- projects/ave/23032022_W01_AVEp0_VEp0/run_scripts/run_single_simulation.py
+++ projects/ave/23032022_W01_AVEp0_VEp0/run_scripts/run_single_simulation.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+
 SCRIPT_DIR = "../../../../"
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
@@ -22,17 +23,18 @@ def run_simulation(path_name):
                         run_options=scan_dict["run_options"],
                         save_options=scan_dict["save_options"])
 
-    sim.simulate(progress_bar=True)
+    sim.simulate(progress_bar=False)
 
 if __name__ == "__main__":
-    # i = int(sys.argv[1])
+    i = int(sys.argv[1])
     # print(i_start,N)
-    i = 980
+    # i = 980
     path_names = open("../scan_summary/23032022_W01_AVEp0_VEp0_path_names.txt").readlines()
     path_name = path_names[i].split("\n")[0]
     t0 = time.time()
     run_simulation(path_name)
     t1 = time.time()
+    print(t1-t0)
     out_file = open("../scan_summary/23032022_W01_AVEp0_VEp0_result_log.txt","a")
     out_file.write("%s_%.2f"%(path_name,(t1-t0)) + "\n")
     out_file.close()
