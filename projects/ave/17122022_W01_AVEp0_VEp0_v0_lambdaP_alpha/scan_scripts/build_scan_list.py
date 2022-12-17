9914b71e871ca3d26d4e0b5f3c0228c5b65d0534
jakesorel <jake.cornwallscoones@crick.ac.uk>
2022-12-17 20:54:17 +0100

    ave_branch2

--- projects/ave/17122022_W01_AVEp0_VEp0_v0_lambdaP_alpha/scan_scripts/build_scan_list.py
+++ projects/ave/17122022_W01_AVEp0_VEp0_v0_lambdaP_alpha/scan_scripts/build_scan_list.py
@@ -56,74 +56,80 @@ if not os.path.exists("../scan_summary/%s_path_names.txt"%base_name):
 #                         for j, seed in enumerate(seed_range):
 j, k, Nper = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
 i = j + k * Nper
-[i1,i2,i3,i4,i5,i6,j] = np.array(list(np.base_repr(i,N).zfill(7))).astype(int)
+[i4,i5,i6,j] = np.array(list(np.base_repr(i,N).zfill(4))).astype(int)
 
 
-counter = i
-W01 = W01_range[i1]
-AVE_p0 = AVE_p0_range[i2]
-VE_p0 = VE_p0_range[i3]
-AVE_v0 = AVE_v0_range[i4]
-lambda_P = lambda_P_range[i5]
-alpha = alpha_range[i6]
-seed = seed_range[j]
+# counter = i
+for i1, W01 in enumerate(W01_range):
+    for i2, AVE_p0 in enumerate(AVE_p0_range):
+        for i3, VE_p0 in enumerate(VE_p0_range):
 
-scan_dict_name = base_name + "_" + "%s"%counter
-df_entry = np.array([counter,W01,AVE_p0,VE_p0,AVE_v0,lambda_P,alpha,seed,scan_dict_name])
+            W01 = W01_range[i1]
+            AVE_p0 = AVE_p0_range[i2]
+            VE_p0 = VE_p0_range[i3]
+            AVE_v0 = AVE_v0_range[i4]
+            lambda_P = lambda_P_range[i5]
+            alpha = alpha_range[i6]
+            seed = seed_range[j]
+            counter = np.flip([i1,i2,i3,i4,i5,i6,j])
+            counter = (N**np.arange(len(counter))*counter).sum()
 
-with open("../scan_summary/%s_full_summary.csv"%base_name, "a") as g:
-    fcntl.flock(g, fcntl.LOCK_EX)
-    g.write(",".join(df_entry.astype(str)) + "\n")
-    fcntl.flock(g, fcntl.LOCK_UN)
+            scan_dict_name = base_name + "_" + "%s"%counter
+            df_entry = np.array([counter,W01,AVE_p0,VE_p0,AVE_v0,lambda_P,alpha,seed,scan_dict_name])
 
-with open("../scan_summary/%s_path_names.txt"%base_name, "a") as g:
-    fcntl.flock(g, fcntl.LOCK_EX)
-    g.write(scan_dict_name + "\n")
-    fcntl.flock(g, fcntl.LOCK_UN)
+            with open("../scan_summary/%s_full_summary.csv"%base_name, "a") as g:
+                fcntl.flock(g, fcntl.LOCK_EX)
+                g.write(",".join(df_entry.astype(str)) + "\n")
+                fcntl.flock(g, fcntl.LOCK_UN)
 
-tissue_params = {"L": 15,
-                 "A0": 1,
-                 "P0": 3.6,
-                 "kappa_A": 1,
-                 "kappa_P": lambda_P,
-                 "W": np.array(((0.0, W01, W01, 0.1), (W01, 0, 0, 0.1), (W01, 0, 0, 0.1),
-                                (0.1, 0.1, 0.1, 0.1))) * 1,
-                 "a": 0,
-                 "k": 0}
-active_params = {"v0": 2e-1,
-                 "Dr": 5e-3}
-init_params = {"init_noise": 0.1,
-               "c_type_proportions": (1.0, 0)}
-run_options = {"equiangulate": True,
-               "equi_nkill": 10}
-simulation_params = {"dt": 0.01,
-                     "tfin": 300,
-                     "tskip": 10,
-                     "dt_grn": 0.025,
-                     "grn_sim": "grn_ave_couple_orientation",
-                     "tinit": 10,
-                     "random_seed": int(seed)}
-grn_params = {"n_AVE_cells": 20,
-              "AVE_alpha_dir": alpha,
-              "non_AVE_alpha_dir": 0,
-              "AVE_v0": AVE_v0,
-              "non_AVE_v0": 0,
-              "AVE_alpha0": -np.pi / 2,
-              "boundary_frac": 0.08,
-              "AVE_A0": 0.54,
-              "exe_frac": 0.0,
-              "AVE_p0": AVE_p0,
-              "nonAVE_p0": VE_p0}
-save_options = {"save": "hdf5",
-                "result_dir": "../scan_results",
-                "name": scan_dict_name,
-                "compressed": True}
+            with open("../scan_summary/%s_path_names.txt"%base_name, "a") as g:
+                fcntl.flock(g, fcntl.LOCK_EX)
+                g.write(scan_dict_name + "\n")
+                fcntl.flock(g, fcntl.LOCK_UN)
 
-scan_dict = {"tissue_params":tissue_params,"active_params":active_params,"init_params":init_params,"run_options":run_options,"simulation_params":simulation_params,"grn_params":grn_params,"save_options":save_options}
+            tissue_params = {"L": 15,
+                             "A0": 1,
+                             "P0": 3.6,
+                             "kappa_A": 1,
+                             "kappa_P": lambda_P,
+                             "W": np.array(((0.0, W01, W01, 0.1), (W01, 0, 0, 0.1), (W01, 0, 0, 0.1),
+                                            (0.1, 0.1, 0.1, 0.1))) * 1,
+                             "a": 0,
+                             "k": 0}
+            active_params = {"v0": 2e-1,
+                             "Dr": 5e-3}
+            init_params = {"init_noise": 0.1,
+                           "c_type_proportions": (1.0, 0)}
+            run_options = {"equiangulate": True,
+                           "equi_nkill": 10}
+            simulation_params = {"dt": 0.01,
+                                 "tfin": 300,
+                                 "tskip": 10,
+                                 "dt_grn": 0.025,
+                                 "grn_sim": "grn_ave_couple_orientation",
+                                 "tinit": 10,
+                                 "random_seed": int(seed)}
+            grn_params = {"n_AVE_cells": 20,
+                          "AVE_alpha_dir": alpha,
+                          "non_AVE_alpha_dir": 0,
+                          "AVE_v0": AVE_v0,
+                          "non_AVE_v0": 0,
+                          "AVE_alpha0": -np.pi / 2,
+                          "boundary_frac": 0.08,
+                          "AVE_A0": 0.54,
+                          "exe_frac": 0.0,
+                          "AVE_p0": AVE_p0,
+                          "nonAVE_p0": VE_p0}
+            save_options = {"save": "hdf5",
+                            "result_dir": "../scan_results",
+                            "name": scan_dict_name,
+                            "compressed": True}
+
+            scan_dict = {"tissue_params":tissue_params,"active_params":active_params,"init_params":init_params,"run_options":run_options,"simulation_params":simulation_params,"grn_params":grn_params,"save_options":save_options}
 
 
-pikd = open("../scan_dicts/%s"%scan_dict_name + ".pickle", 'wb')
-pickle.dump(scan_dict, pikd)
-pikd.close()
-print(counter)
+            pikd = open("../scan_dicts/%s"%scan_dict_name + ".pickle", 'wb')
+            pickle.dump(scan_dict, pikd)
+            pikd.close()
+            print(counter)
 
