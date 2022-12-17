6257476006062fbc60866e39c7e9b69cae8fc077
jakesorel <jake.cornwallscoones@crick.ac.uk>
2022-12-17 21:33:58 +0100

    ave_branch2

--- projects/ave/17122022_W01_AVEp0_VEp0_v0_lambdaP_alpha/scan_scripts/remove_files.sh
+++ projects/ave/17122022_W01_AVEp0_VEp0_v0_lambdaP_alpha/scan_scripts/remove_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Submit this script with: sbatch thefilename
 
-#SBATCH --time=2:00:00   # walltime
+#SBATCH --time=20:00:00   # walltime
 #SBATCH -J "AVE_for_loop_build_lists"   # job name
 #SBATCH --output=../bash_out/for_loop_build_lists_output.out
 #SBATCH --error=../bash_out/for_loop_build_lists_error.out
