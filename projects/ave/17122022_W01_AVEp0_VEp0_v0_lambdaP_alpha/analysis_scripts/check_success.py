3f4561a8aa296a777b82293c9d084b6b8ad1c96e
jakesorel <jake.cornwallscoones@crick.ac.uk>
2023-01-18 23:35:41 +0100

    ave_branch2

--- projects/ave/17122022_W01_AVEp0_VEp0_v0_lambdaP_alpha/analysis_scripts/check_success.py
+++ projects/ave/17122022_W01_AVEp0_VEp0_v0_lambdaP_alpha/analysis_scripts/check_success.py
@@ -1,7 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import pandas as pd
 
 
 
@@ -24,31 +24,31 @@ not_indexed = list(set(list(index.ravel())).difference(set(numbers)))
 fl = open("../scan_summary/not_run.txt","w")
 fl.write("\n".join(np.array(not_indexed).astype(str)))
 fl.close()
-
-df = pd.DataFrame({"W01":W01.ravel()[numbers],
-                   "AVE_p0":AVE_p0.ravel()[numbers],
-                   "VE_p0":VE_p0.ravel()[numbers],
-                   "AVE_v0":AVE_v0.ravel()[numbers],
-                   "lambda_P":lambda_P.ravel()[numbers],
-                   "seed":seed.ravel()[numbers]})
-
-sns.pairplot(data=df)
-plt.show()
-
-
-fig, ax = plt.subplots(5,5)
-for i in range(5):
-    for j in range(5):
-        if i > j:
-            ax[j,i].imshow(indexed.sum(axis=tuple(set(np.arange(6)).difference([i,j]))),vmin=0,vmax=10000)
-fig.show()
-
-
-"""
-    W01 = W01_range[i1]
-    AVE_p0 = AVE_p0_range[i2]
-    VE_p0 = VE_p0_range[i3]
-    AVE_v0 = AVE_v0_range[i4]
-    lambda_P = lambda_P_range[i5]
-    seed = seed_range[j]
-"""
+#
+# df = pd.DataFrame({"W01":W01.ravel()[numbers],
+#                    "AVE_p0":AVE_p0.ravel()[numbers],
+#                    "VE_p0":VE_p0.ravel()[numbers],
+#                    "AVE_v0":AVE_v0.ravel()[numbers],
+#                    "lambda_P":lambda_P.ravel()[numbers],
+#                    "seed":seed.ravel()[numbers]})
+#
+# sns.pairplot(data=df)
+# plt.show()
+#
+#
+# fig, ax = plt.subplots(5,5)
+# for i in range(5):
+#     for j in range(5):
+#         if i > j:
+#             ax[j,i].imshow(indexed.sum(axis=tuple(set(np.arange(6)).difference([i,j]))),vmin=0,vmax=10000)
+# fig.show()
+#
+#
+# """
+#     W01 = W01_range[i1]
+#     AVE_p0 = AVE_p0_range[i2]
+#     VE_p0 = VE_p0_range[i3]
+#     AVE_v0 = AVE_v0_range[i4]
+#     lambda_P = lambda_P_range[i5]
+#     seed = seed_range[j]
+# """
