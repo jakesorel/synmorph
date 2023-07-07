fed878f6b96b0b0f4a87d4db8c43d2e9e2a42b10
cornwaj <jakesorel@rocketmail.com>
2023-07-07 09:47:58 +0100

    ave_branch_updated

--- projects/ave/06072023_W01_AVEp0_VEp0/run_scripts/run_simulation.sh
+++ projects/ave/06072023_W01_AVEp0_VEp0/run_scripts/run_simulation.sh
@@ -7,10 +7,28 @@
 #SBATCH -n 1
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=20G
+#SBATCH --mem=100G
 
 eval "$(conda shell.bash hook)"
 source activate synmorph
 
-python run_multiple_simulation.py ${SLURM_ARRAY_TASK_ID}
+while true; do
+    python python run_multiple_simulation.py ${SLURM_ARRAY_TASK_ID}
+    exit_code=$?
+
+    if [ $exit_code -eq 0 ]; then
+        # Script completed successfully
+        echo "Script completed successfully"
+        break
+    elif [ $exit_code -eq 1 ]; then
+        # Restart the script due to TerminatedWorkerError
+        echo "Restarting script..."
+    else
+        # Other non-zero exit code, handle as needed
+        echo "Script encountered an error. Exiting..."
+        break
+    fi
+done
+
+
 #python ../analysis_scripts/run_analysis.py ${SLURM_ARRAY_TASK_ID}
