a37df1defa6382624414323b85db0337eadbe998
cornwaj <jakesorel@rocketmail.com>
2023-07-23 15:42:06 -0700

    ave_branch_updated

--- projects/ave/22072023_W01_AVEp0_VEp0/run_scripts/check_remaining_to_run.py
+++ projects/ave/22072023_W01_AVEp0_VEp0/run_scripts/check_remaining_to_run.py
@@ -12,4 +12,20 @@ df_out = pd.DataFrame({"to_run":to_run_idx})
 df_out.to_csv("../scan_summary/to_run.csv")
 print(df_out.shape)
 
+df_t = pd.read_csv("../analysis_results/t_hit_boundary.txt",header=None)
+df_a = pd.read_csv("../scan_summary/analysis_files.txt",header=None)
+t_index = [int(nm.split("_")[4]) for nm in df_t[0]]
+a_index = [int(nm.split("_")[4]) for nm in df_a[0]]
+both_index = list(set(a_index).intersection(set(t_index)))
+
+is_in_a = np.zeros(20**4,dtype=int)
+for i in both_index:
+    is_in_a[i] = 1
+
+to_run_a_idx = np.nonzero(1-is_in_a)[0]
+df_out_a = pd.DataFrame({"to_run":to_run_a_idx})
+df_out_a.to_csv("../scan_summary/to_run_analysis.csv")
+print(df_out_a.shape)
+
+
 
---
+++ projects/ave/22072023_W01_AVEp0_VEp0/run_scripts/for_loop_remaining_analysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=01:00:00   # walltime
+#SBATCH -J "AVE2_analysis_for_loop"   # job name
+#SBATCH --output=../bash_out/AVE2_analysis_for_loop.out
+#SBATCH --error=../bash_out/AVE2_analysis_for_loop.out
+#SBATCH -n 1
+#SBATCH --partition=cpu
+#SBATCH --mem=2G
+
+
+
+sbatch --array [0-6347] run_remaining_analysis.sh ${SLURM_ARRAY_TASK_ID}
+
+
