85d5acf23c3bc7b8facbaed3a20a2d2f5a731e26
cornwaj <jakesorel@rocketmail.com>
2023-08-30 16:24:07 -0700

    ave_branch_updated

--- projects/ave/22072023_W01_AVEp0_VEp0/run_scripts/for_loop_remaining.sh
+++ projects/ave/22072023_W01_AVEp0_VEp0/run_scripts/for_loop_remaining.sh
@@ -12,5 +12,5 @@
 
 
 
-sbatch --array [37285] --output=../bash_out/output_%A_%a.out --error=../bash_out/error_%A_%a.out run_remaining_simulation.sh ${SLURM_ARRAY_TASK_ID}
+sbatch --array [37285-37285] --output=../bash_out/output_%A_%a.out --error=../bash_out/error_%A_%a.out run_remaining_simulation.sh ${SLURM_ARRAY_TASK_ID}
 
