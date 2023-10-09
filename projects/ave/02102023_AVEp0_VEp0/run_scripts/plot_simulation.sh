5b121bb98a6f1ee48b737230b184ceb08a3553f2
cornwaj <jakesorel@rocketmail.com>
2023-10-09 12:39:22 +0100

    ave_branch_updated

--- projects/ave/02102023_AVEp0_VEp0/run_scripts/plot_simulation.sh
+++ projects/ave/02102023_AVEp0_VEp0/run_scripts/plot_simulation.sh
@@ -2,14 +2,14 @@
 
 #Submit this script with: sbatch thefilename
 
-#SBATCH --time=00-00:30:00   # walltime
+#SBATCH --time=01-00:00:00   # walltime
 #SBATCH -J "cpu_count"   # job name
 #SBATCH -n 1
 #SBATCH --partition=cpu
 #SBATCH --output=../bash_out/plot_sim_output.out
 #SBATCH --error=../bash_out/plot_sim_error.out
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
 
 eval "$(conda shell.bash hook)"
 source activate synmorph
