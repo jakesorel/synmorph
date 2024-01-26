2bb03ac4605c0747db789210e25b100a26d878b5
cornwaj <jakesorel@rocketmail.com>
2024-01-26 18:22:24 +0000

    heart_stretch

--- projects/heartStretch/26012023_followup_screen/run_scripts/run_simulation.sh
+++ projects/heartStretch/26012023_followup_screen/run_scripts/run_simulation.sh
@@ -3,19 +3,22 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=23:59:00   # walltime
-#SBATCH -J "heart_simulations_initialisation"   # job name
+#SBATCH -J "heart_simulations"   # job name
 #SBATCH -n 1
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
-#SBATCH --output=../bash_out/error_initialisation.out
-#SBATCH --error=../bash_out/output_initialisation.out
+
 
 eval "$(conda shell.bash hook)"
 source activate synmorph
 
-python mcmc_randomiser.py
+python run_multiple_simulation.py ${SLURM_ARRAY_TASK_ID}
 
+
+
+
+#python ../analysis_scripts/run_analysis.py ${SLURM_ARRAY_TASK_ID}
 
 
 
